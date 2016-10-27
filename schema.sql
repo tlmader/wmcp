25989f2ada2b11ee67838da4019ff6be6e604027
Ted Mader <tlmader.dev@gmail.com>
2016-10-27 13:58:03 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,43 @@
-CREATE TABLE address
-(
-  address_id VARCHAR2(10) NOT NULL PRIMARY KEY,
-  street VARCHAR2(100) NOT NULL,
-  locality VARCHAR2(100) NOT NULL,
-  region VARCHAR2(100) NOT NULL,
-  zip_code VARCHAR2(10) NOT NULL,
-  country_code VARCHAR2(2) NOT NULL
-);
-CREATE TABLE person
-(
-  per_id VARCHAR2(10) NOT NULL PRIMARY KEY,
-  person_name VARCHAR2(100) NOT NULL,
-  address_id VARCHAR2(10),
-  email VARCHAR2(100),
-  gender VARCHAR2(100),
-  CONSTRAINT fk_per_address FOREIGN KEY (address_id)
-    REFERENCES address(address_id)
-);
-CREATE TABLE phone
-(
-  per_id VARCHAR2(10) NOT NULL PRIMARY KEY,
-  phone_num VARCHAR2(15) NOT NULL,
-  CONSTRAINT fk_phone_per FOREIGN KEY (per_id)
-    REFERENCES person(per_id)
-);
+BEGIN
+   FOR cur_rec IN (SELECT object_name, object_type
+                     FROM user_objects
+                    WHERE object_type IN
+                             ('TABLE',
+                              'VIEW',
+                              'PACKAGE',
+                              'PROCEDURE',
+                              'FUNCTION',
+                              'SEQUENCE'
+                             ))
+   LOOP
+      BEGIN
+         IF cur_rec.object_type = 'TABLE'
+         THEN
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '" CASCADE CONSTRAINTS';
+         ELSE
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '"';
+         END IF;
+      EXCEPTION
+         WHEN OTHERS
+         THEN
+            DBMS_OUTPUT.put_line (   'FAILED: DROP '
+                                  || cur_rec.object_type
+                                  || ' "'
+                                  || cur_rec.object_name
+                                  || '"'
+                                 );
+      END;
+   END LOOP;
+END;
+/
 CREATE TABLE job_profile
 (
   jp_code VARCHAR2(10) NOT NULL PRIMARY KEY,
@@ -55,18 +69,41 @@ CREATE TABLE company
   address_id VARCHAR2(10),
   primary_sector VARCHAR2(100),
   website VARCHAR2(100),
-  CONSTRAINT fk_comp_address FOREIGN KEY (address_id)
-    REFERENCES address(address_id)
+  street VARCHAR2(100) NOT NULL,
+  locality VARCHAR2(100) NOT NULL,
+  region VARCHAR2(100) NOT NULL,
+  zip_code VARCHAR2(10) NOT NULL,
+  country_code VARCHAR2(2) NOT NULL
 );
 CREATE TABLE job
 (
   job_code VARCHAR2(10) NOT NULL PRIMARY KEY,
+  jp_code VARCHAR2(10) NOT NULL,
+  comp_id VARCHAR2(10) NOT NULL,
   type VARCHAR2(100) NOT NULL,
   pay_rate NUMBER(20),
   pay_type NUMBER(20),
-  comp_id VARCHAR2(10) NOT NULL,
   CONSTRAINT fk_job_comp FOREIGN KEY (comp_id)
-    REFERENCES company(comp_id)
+    REFERENCES company(comp_id),
+  CONSTRAINT fk_job_jp FOREIGN KEY (jp_code)
+    REFERENCES job_profile(jp_code)
+);
+CREATE TABLE person
+(
+  per_id VARCHAR2(10) NOT NULL PRIMARY KEY,
+  job_code VARCHAR2(10) NOT NULL,
+  person_name VARCHAR2(100) NOT NULL,
+  address_id VARCHAR2(10),
+  phone_num VARCHAR2(15) NOT NULL,
+  email VARCHAR2(100),
+  gender VARCHAR2(100),
+  street VARCHAR2(100) NOT NULL,
+  locality VARCHAR2(100) NOT NULL,
+  region VARCHAR2(100) NOT NULL,
+  zip_code VARCHAR2(10) NOT NULL,
+  country_code VARCHAR2(2) NOT NULL,
+  CONSTRAINT fk_per_id FOREIGN KEY (job_code)
+    REFERENCES job(job_code)
 );
 CREATE TABLE specialty
 (
@@ -94,14 +131,43 @@ CREATE TABLE course
 );
 CREATE TABLE section
 (
+  sec_no VARCHAR2(10) NOT NULL PRIMARY KEY,
   c_code VARCHAR2(10) NOT NULL,
-  sec_no VARCHAR2(10) NOT NULL,
   complete_date DATE,
   sec_year NUMBER(4),
   offered_by VARCHAR2(100),
   sec_format VARCHAR2(100),
   price NUMBER(20),
-  CONSTRAINT pk_section PRIMARY KEY (c_code, sec_no),
   CONSTRAINT fk_sec_c FOREIGN KEY (c_code)
     REFERENCES course(c_code)
+);
+CREATE TABLE takes
+(
+  per_id VARCHAR2(10) NOT NULL,
+  sec_no VARCHAR2(10) NOT NULL,
+  CONSTRAINT pk_takes PRIMARY KEY (per_id, sec_no),
+  CONSTRAINT fk_takes_per FOREIGN KEY (per_id)
+    REFERENCES person(per_id),
+  CONSTRAINT fk_takes_sec FOREIGN KEY (sec_no)
+    REFERENCES section(sec_no)
+);
+CREATE TABLE knows
+(
+  per_id VARCHAR2(10) NOT NULL,
+  ks_code VARCHAR2(10) NOT NULL,
+  CONSTRAINT pk_knows PRIMARY KEY (per_id, ks_code),
+  CONSTRAINT fk_knows_per FOREIGN KEY (per_id)
+    REFERENCES person(per_id),
+  CONSTRAINT fk_knows_ks FOREIGN KEY (ks_code)
+    REFERENCES knowledge_skill(ks_code)
+);
+CREATE TABLE teaches
+(
+  c_code VARCHAR2(10) NOT NULL,
+  ks_code VARCHAR2(10) NOT NULL,
+  CONSTRAINT pk_teaches PRIMARY KEY (c_code, ks_code),
+  CONSTRAINT fk_teaches_c FOREIGN KEY (c_code)
+    REFERENCES course(c_code),
+  CONSTRAINT fk_teaches_ks FOREIGN KEY (ks_code)
+    REFERENCES knowledge_skill(ks_code)
 );
