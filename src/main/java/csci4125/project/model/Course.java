d725908b8a0d06cf6de93e0cd040b35e74c0ebf5
Ted Mader <ted.mader@ge.com>
2016-11-17 13:33:31 -0600

    Add column annotations and level field to Course model

--- src/main/java/csci4125/project/model/Course.java
+++ src/main/java/csci4125/project/model/Course.java
@@ -1,7 +1,7 @@
 package csci4125.project.model;
 
-import csci4125.project.repository.impl.CourseRepository;
-
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.Currency;
 
@@ -12,17 +12,24 @@ import java.util.Currency;
  * @since 2016-11-17
  */
 @Entity
+@AttributeOverride(name = "id", column = @Column(name = "c_code"))
 public class Course extends Model {
 
-    private String id;
+    @Column(name = "c_title")
     private String title;
+    @Column(name = "c_level")
+    private String level;
+    @Column(name = "description")
     private String description;
+    @Column(name = "status")
     private String status;
+    @Column(name = "retail_price")
     private Currency price;
 
-    public Course(String id, String title, String description, String status, Currency price) {
+    public Course(String id, String title, String level, String description, String status, Currency price) {
         super(id);
         this.title = title;
+        this.level = level;
         this.description = description;
         this.status = status;
         this.price = price;
@@ -34,6 +41,14 @@ public class Course extends Model {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
     }
 
     public String getDescription() {
@@ -58,5 +73,29 @@ public class Course extends Model {
 
     public void setPrice(Currency price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+
+        Course course = (Course) o;
+
+        if (title != null ? !title.equals(course.title) : course.title != null) return false;
+        if (level != null ? !level.equals(course.level) : course.level != null) return false;
+        if (description != null ? !description.equals(course.description) : course.description != null) return false;
+        if (status != null ? !status.equals(course.status) : course.status != null) return false;
+        return price != null ? price.equals(course.price) : course.price == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (level != null ? level.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
     }
 }
