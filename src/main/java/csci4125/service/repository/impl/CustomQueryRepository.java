6320d3a8a8925a53698c119e06446a0dbab54e77
Ted Mader <tlmader.dev@gmail.com>
2016-11-20 11:58:44 -0600

    Add CustomQueryRepository

---
+++ src/main/java/csci4125/service/repository/impl/CustomQueryRepository.java
@@ -0,0 +1,36 @@
+package csci4125.service.repository.impl;
+
+import csci4125.service.repository.ICustomQueryRepository;
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Implements data access methods for the usage of native queries.
+ *
+ * @author tlmader.dev@gmail.com
+ * @since 2016-11-20
+ */
+@Component
+public class CustomQueryRepository implements ICustomQueryRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Object[]> get(String query) {
+        SQLQuery sqlQuery = sessionFactory.getCurrentSession().createSQLQuery(query);
+        List results = sqlQuery.list();
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results;
+    }
+}
