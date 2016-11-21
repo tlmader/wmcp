641a8c4521f8cfd8aab19f6e56d6eee05d3546d5
Ted Mader <tlmader.dev@gmail.com>
2016-11-21 01:27:46 -0600

    Add Person API

---
+++ src/main/java/csci4125/application/service/impl/PersonService.java
@@ -0,0 +1,21 @@
+package csci4125.application.service.impl;
+
+import csci4125.application.model.Person;
+import csci4125.application.repository.ICrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements manager workflow methods for Person entities.
+ *
+ * @author tlmader.dev@gmail.com
+ * @since 11/20/2016
+ */
+@Service
+public class PersonService extends CrudService<Person> {
+
+    @Autowired
+    public PersonService(ICrudRepository<Person> repository) {
+        super(repository);
+    }
+}
