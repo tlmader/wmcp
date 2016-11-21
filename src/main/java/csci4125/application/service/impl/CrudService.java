0efa0fd4dc35295b2f726e1645ba2c2502725442
Ted Mader <tlmader.dev@gmail.com>
2016-11-20 23:25:34 -0600

    Complete CrudService impl

--- src/main/java/csci4125/application/service/impl/CrudService.java
+++ src/main/java/csci4125/application/service/impl/CrudService.java
@@ -1,20 +1,13 @@
 package csci4125.application.service.impl;
 
-import java.util.List;
-
-import javax.ws.rs.ClientErrorException;
-import javax.ws.rs.core.Response;
-
 import csci4125.application.model.Model;
 import csci4125.application.repository.ICrudRepository;
 import csci4125.application.service.ICrudService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ge.digital.csci4125.project.managers.ICrudManager;
-import com.ge.digital.csci4125.project.repository.IResourceAccessor;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Implements manager workflow methods for T entities.
@@ -23,11 +16,11 @@ import com.ge.digital.csci4125.project.repository.IResourceAccessor;
  * @since 2016-11-20
  */
 @Service
-public class CrudService<T extends Model> implements ICrudService<T> {
+abstract class CrudService<T extends Model> implements ICrudService<T> {
 
-    private ICrudRepository<T> repository
+    private ICrudRepository<T> repository;
 
-    public TManager(ICrudRepository<T> repository) {
+    public CrudService(ICrudRepository<T> repository) {
         this.repository = repository;
     }
 
@@ -47,19 +40,13 @@ public class CrudService<T extends Model> implements ICrudService<T> {
 
     @Override
     public T create(T entity) {
-            return this.repository.create(entity);
-        } else {
-            return this.repository.update(entity);
-        }
+        return this.repository.create(entity);
     }
 
     @Override
     public T update(T entity) {
-        return this.repository.create(entity);
-    } else {
         return this.repository.update(entity);
-        }
-        }
+    }
 
     @Override
     public void delete(String id) {
