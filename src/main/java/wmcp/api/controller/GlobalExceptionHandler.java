6062039a64a52cb0579bf2a6a99af3b1cc37772b
Ted Mader <tlmader.dev@gmail.com>
2016-11-26 13:40:31 -0600

    Rename ClientErrorExceptionHandler to GlobalExceptionHandler, update env usage in application.properties, and disable incomplete impl

--- src/main/java/wmcp/api/controller/GlobalExceptionHandler.java
+++ src/main/java/wmcp/api/controller/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.ClientErrorException;
+import java.sql.SQLException;
 
 /**
  * Handles errors and builds responses for ClientErrorExceptions.
@@ -15,13 +16,20 @@ import javax.ws.rs.ClientErrorException;
  * @since 2016-11-25
  */
 @ControllerAdvice
-public class ClientErrorExceptionHandler {
+public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(ClientErrorExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ClientErrorException.class)
+    public String handleClientErrorException(HttpServletRequest request, Exception ex) {
+        String message = "ClientErrorException occurred:: URL=" + request.getRequestURL();
+        logger.info(message);
+        return message;
+    }
+
+    @ExceptionHandler(SQLException.class)
     public String handleSQLException(HttpServletRequest request, Exception ex) {
-        String message = "ClientErrorException Occured:: URL=" + request.getRequestURL();
+        String message = "SQLException occurred:: URL=" + request.getRequestURL();
         logger.info(message);
         return message;
     }
