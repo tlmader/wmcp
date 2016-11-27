8ebdb6fc838e1be55262a3f189b0ff8ea0a15462
Ted Mader <tlmader.dev@gmail.com>
2016-11-26 18:16:54 -0600

    Remove custom mapper configs

--- src/main/java/wmcp/api/config/AppConfig.java
+++ src/main/java/wmcp/api/config/AppConfig.java
@@ -2,12 +2,8 @@ package wmcp.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import java.util.List;
 
 /**
  * Defines Beans and configurations.
@@ -21,19 +17,5 @@ public class AppConfig extends WebMvcConfigurationSupport {
     @Bean
     public HibernateJpaSessionFactoryBean createSessionFactory() {
         return new HibernateJpaSessionFactoryBean();
-    }
-
-    @Bean
-    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        HibernateAwareObjectMapper mapper = new HibernateAwareObjectMapper();
-        converter.setObjectMapper(mapper);
-        return converter;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(customJackson2HttpMessageConverter());
-        super.addDefaultHttpMessageConverters(converters);
     }
 }
